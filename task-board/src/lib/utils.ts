d599cc7152d62bba38efdfcafa42334ec57dc14a
Halil Teyfik <hlltvfk@gmail.com>
2025-12-12 00:55:03 +0300

    chore(task-board): install @tw-classed/react

--- task-board/src/lib/utils.ts
+++ task-board/src/lib/utils.ts
@@ -1,6 +1,9 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { createClassed } from "@tw-classed/react";
+import { type ClassValue, clsx } from "clsx";
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
 }
+
+export const { classed } = createClassed({ merger: twMerge });
